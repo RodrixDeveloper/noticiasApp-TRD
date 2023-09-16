1a5d944c3be101ccacc033fe604ffe4f1213c958
RodrigoCG <rodrig127g@gmail.com>
2023-09-16 19:56:59 -0400

    aplicacion de noticias con newsApi

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
+import { NoticiaService } from './services/noticia.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'noticiasApp';
+  title:string = 'noticiasApp';
+  listNoticias:any []= []
+  loading:boolean = false;
+
+  constructor(private _noticiaService:NoticiaService){}
+
+  buscarNoticias(parametros: any) {
+    console.log("PADRE");
+    console.log(parametros);
+    this.loading=true;
+    this.listNoticias=[];
+    setTimeout(() => {
+      this._noticiaService.getNoticias(parametros).subscribe(data=>{
+        this.loading=false;
+        console.log(data.articles);
+        this.listNoticias = data.articles
+      }, error=>{
+        console.log(error);
+        this.loading=false
+      })
+    }, 1500);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule} from '@angular/common/http'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormularioComponent } from './components/formulario/formulario.component';
+import { ListadoNoticiasComponent } from './components/listado-noticias/listado-noticias.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { SharedSpinnerComponent } from './shared-spinner/shared-spinner.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormularioComponent,
+    ListadoNoticiasComponent,
+    NavbarComponent,
+    SharedSpinnerComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
